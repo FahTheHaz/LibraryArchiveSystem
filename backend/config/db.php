a71f58005d94964e4bdad65063b42d1778357ddb
FahTheHaz <fahimhaziq1@gmail.com>
2026-02-28 10:00:32 +0800

    Added connections and test

--- backend/config/db.php
+++ backend/config/db.php
@@ -5,10 +5,15 @@ $username = "root";
 $password = "";
 $database = "library_archive";
 
-$conn = new mysqli($servername, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die("connection failed");
+try{
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
 }
+catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+
 
 ?>
